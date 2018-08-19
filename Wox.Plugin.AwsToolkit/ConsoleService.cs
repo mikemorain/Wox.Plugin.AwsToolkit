5ddfea42eaa4fb49dff85ee07695f30b7baa1a3b
Mike Morain <mike.morain+git@gmail.com>
2018-08-19 18:46:43 +0100

    adding images and plugin files

---
+++ Wox.Plugin.AwsToolkit/ConsoleService.cs
@@ -0,0 +1,14 @@
+﻿using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+
+namespace Wox.Plugin.AwsToolkit
+{
+    public class ConsoleService
+    {
+        [YamlMember(Alias = "command", ApplyNamingConventions = false)]
+        public string Title { get; set;  }
+        public string Url { get; set; }
+        [YamlMember(Alias = "description", ApplyNamingConventions = false)]
+        public string Subtitle { get; set; }
+    }
+}
--- Wox.Plugin.AwsToolkit/Main.cs
+++ Wox.Plugin.AwsToolkit/Main.cs
@@ -1,42 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
 namespace Wox.Plugin.AwsToolkit
 {
-    public class AwsConsoleServicesPlugin : IPlugin
+    public class Main: IPlugin
     {
+        private List<ConsoleService> _services;
+        private string _pluginDirectory;
+        
         public List<Result> Query(Query query)
         {
             var results = new List<Result>();
-            
-            results.Add();
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(query.Search)) return results;
+            var keyword = query.Search;
+
+            var searchList = _services.Where(x => x.Title.ToLower().Contains(keyword));
+            foreach (var s in searchList)
+            {
+                var r = new Result();
+                r.Title = s.Title;
+                r.SubTitle = s.Subtitle;
+                r.IcoPath = $"{_pluginDirectory}\\images\\{s.Title}.png";
+                r.Action = c =>
+                {
+                    try
+                    {
+                        Process.Start(s.Url);
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                };
+                r.ContextData = s;
+                results.Add(r);
+            }
+
+            return results;
         }
 
         public void Init(PluginInitContext context)
         {
+            // 
+            _pluginDirectory = context.CurrentPluginMetadata.PluginDirectory;
             // read in the yaml
             var deserializer = new DeserializerBuilder()
                 .WithNamingConvention(new CamelCaseNamingConvention())
                 .Build();
-            using (Stream stream = File.OpenRead("console-services.yml"))
+            using (Stream stream = File.OpenRead($"{_pluginDirectory}\\console-services.yml"))
             {
                 using (TextReader reader = new StreamReader(stream))
                 {
-                    var services = deserializer.Deserialize<List<ConsoleService>>(reader);
-                    foreach (var s in services)
-                    {
-                        Console.WriteLine(s.Title);
-                    }
+                    _services = deserializer.Deserialize<List<ConsoleService>>(reader);
                 }
 
             }
-            
-            // 
-            throw new NotImplementedException();
         }
     }
 }
